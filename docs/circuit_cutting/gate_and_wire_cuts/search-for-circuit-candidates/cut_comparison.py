fc631fc0de332219951ff50f199e3c33e7e37ed2
Filip Mazurek <22063152+filipmazurek@users.noreply.github.com>
2024-10-06 20:36:50 -0400

    Searching for cut candidates

--- docs/circuit_cutting/gate_and_wire_cuts/search-for-circuit-candidates/cut_comparison.py
+++ docs/circuit_cutting/gate_and_wire_cuts/search-for-circuit-candidates/cut_comparison.py
@@ -5,7 +5,7 @@ from circuit_knitting.cutting.gate_and_wire_cutting.cutting import cut_circuit_g
 from circuit_knitting.cutting.gate_and_wire_cutting.util import copy_and_add_ancilla, cut_list_to_wire_and_gates
 
 
-def cut_comparison(circuit, max_cuts, num_subcircuits, max_subcircuit_width):
+def _cut_comparison(circuit, max_cuts, num_subcircuits, max_subcircuit_width):
     # CutQC
     subcircuits_cutqc = cut_circuit_wires(
         circuit=circuit,
@@ -36,14 +36,35 @@ def cut_comparison(circuit, max_cuts, num_subcircuits, max_subcircuit_width):
 def _example_use():
     from qiskit.circuit.library import EfficientSU2
 
-    # Create a quantum circuit to cut. We create a simple ansatz
-    ansatz = EfficientSU2(3, reps=1)
-    # Decompose to the actual individual gates
-    circuit = ansatz.decompose(reps=3)
-    # Set some arbitrary parameters
-    circuit.assign_parameters([0.8] * len(circuit.parameters), inplace=True)
+    circuits = []
+    for val in range(0.1, 1.0, 0.1):
+        # Create a quantum circuit to cut. We create a simple ansatz
+        ansatz = EfficientSU2(3, reps=1)
+        # Decompose to the actual individual gates
+        circuit = ansatz.decompose(reps=3)
+        # Set some arbitrary parameters
+        circuit.assign_parameters([0.8] * len(circuit.parameters), inplace=True)
 
-    print(cut_comparison(circuit, 4, [2], 2))
+    print(_cut_comparison(circuit, 4, [2], 2))
+
+
+def compare_circuit_cuts(circuits, max_cuts, num_subcircuits, max_subcircuit_width, file_name=None):
+    if file_name is None:
+        raise ValueError("Please provide a file name to save the results")
+
+    results = []
+
+    # Copmpare the number of cuts in CutQC and against gate and wire cutting
+    for circuit in circuits:
+        results.append(_cut_comparison(circuit, max_cuts, num_subcircuits, max_subcircuit_width))
+
+    # Save all results to a CSV
+    import csv
+    with open(file_name, mode='w') as file:
+        writer = csv.writer(file)
+        writer.writerow(["CutQC", "Gate and Wire"])
+        for result in results:
+            writer.writerow(result)
 
 
 if __name__ == "__main__":
